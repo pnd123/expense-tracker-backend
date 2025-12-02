a08cf31c5e9a6e4eaedc45397686ced5722a5936
Prasann Daddikar <prasanndaddikar333@gmail.com>
2025-12-02 18:47:49 +0530

    Refactor test class to main application class
    Refactor BackendApplicationTests to BackendApplication and add main method to load environment variables.

--- src/test/java/com/expensetracker/backend/BackendApplicationTests.java
+++ src/test/java/com/expensetracker/backend/BackendApplicationTests.java
@@ -1,13 +1,23 @@
 package com.expensetracker.backend;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
-@SpringBootTest
-class BackendApplicationTests {
-
-	@Test
-	void contextLoads() {
+@SpringBootApplication
+public class BackendApplication {
+	
+	 
+	public static void main(String[] args) {
+		
+		// Load .env file
+        Dotenv dotenv = Dotenv.load();
+        // Set environment variables for Spring Boot
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
